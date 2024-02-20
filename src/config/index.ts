e95da28e5cd1469592a0185f32ea4de9d0f825d4
patrykharasiuk <patryk.harasiuk@op.pl>
2024-02-20 23:14:53 +0100

    feat: add compressed logo image

---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+export const ABOUT_ME = `O mnie
+EsteticHairStudio to miejsce, w którym profesjonalnie i bardzo starannie zajmę się Twoimi włosami. Oferuję szeroki wachlarz usług fryzjersko-pielęgnacyjnych w najwyższym standardzie. Gwarantuję bezpieczeństwo i pełne zadowolenie z wykonanej przeze mnie pracy.
+Świadczę takie usługi jak keratynowe prostowanie włosów, glow hair, rekonstrukcja na zimno, botox i inne zabiegi pielęgnacyjne. Podczas pracy korzystam z profesjonalnych produktów najwyższej jakości. Jestem po wielokrotnych szkoleniach od najlepszych specjalistów w Polsce.
+Poznaj mnie bliżej w moich social mediach`;
