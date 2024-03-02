f523755dcaf3002362a8738a29c83131bcc4d241
patrykharasiuk <patryk.harasiuk@op.pl>
2024-03-02 22:32:15 +0100

    feat: add dismissing for clicking on backdrop

--- src/layouts/components/Drawer.tsx
+++ src/layouts/components/Drawer.tsx
@@ -17,18 +17,20 @@ export const Drawer = ({ children, handleDismiss }: Props) => {
     <Portal>
       <ReactFocusLock returnFocus={false}>
         <div className="fixed inset-0 p-4">
-          <div className="absolute inset-0 bg-black/40 backdrop-blur-sm animate-fadeIn">
-            <div className="absolute top-0 right-0 bottom-0 w-3/4 sm:w-1/2 max-w-[280px] min-w-[200px] flex flex-col bg-white p-8 animate-slideIn">
-              <button
-                className="absolute top-2 right-4 flex text-lg items-center justify-center p-2 cursor-pointer border-none"
-                onClick={handleDismiss}
-              >
-                <CloseSVG />
-                <VisuallyHidden>Close menu</VisuallyHidden>
-              </button>
+          <div
+            onClick={handleDismiss}
+            className="absolute inset-0 bg-black/40 backdrop-blur-sm animate-fadeIn"
+          />
+          <div className="absolute top-0 right-0 bottom-0 w-3/4 sm:w-1/2 max-w-[280px] min-w-[200px] flex flex-col bg-white p-8 animate-slideIn">
+            <button
+              className="absolute top-2 right-4 flex text-lg items-center justify-center p-2 cursor-pointer border-none"
+              onClick={handleDismiss}
+            >
+              <CloseSVG />
+              <VisuallyHidden>Close menu</VisuallyHidden>
+            </button>
 
-              <div className="pt-7">{children}</div>
-            </div>
+            <div className="pt-7">{children}</div>
           </div>
         </div>
       </ReactFocusLock>
